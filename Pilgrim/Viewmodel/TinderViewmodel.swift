95d5838c15d66bd9de37001eaefc3e669bf5c16e
Paulo Henrique <phsilva098@gmail.com>
2023-06-21 11:42:12 -0300

    refactor: reading from userdefaults in tinderviewmodel

--- Pilgrim/Viewmodel/TinderViewmodel.swift
+++ Pilgrim/Viewmodel/TinderViewmodel.swift
@@ -32,6 +32,14 @@ class TinderViewmodel: ObservableObject {
 
     init() {
         getShuffleData()
+        let correctData = UserDefaults.standard.data(forKey: "correctLocal")
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode([LocalElement].self, from: correctData!)
+            correctLocals = decodedData
+        } catch {
+            print(error)
+        }
     }
 
     func getShuffleData() {
