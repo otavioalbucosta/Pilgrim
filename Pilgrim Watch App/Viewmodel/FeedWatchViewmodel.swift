23f936029cc221881af24fa3a6a792557d672c3f
Paulo Henrique <phsilva098@gmail.com>
2023-06-23 11:29:41 -0300

    feat: sending data from iphone to watch

---
+++ Pilgrim Watch App/Viewmodel/FeedWatchViewmodel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedWatchViewmodel.swift
+//  Pilgrim Watch App
+//
+//  Created by Paulo Henrique Gomes da Silva on 23/06/23.
+//
+
+import Foundation
+
+class FeedWatchViewmdodel: ObservableObject {
+    
+}
