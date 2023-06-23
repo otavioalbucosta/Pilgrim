5a76e318e45356b2c3c67b8b1f0e77498ab4654e
Luiz Sena <senaluiz1307@gmail.com>
2023-06-23 11:39:22 -0300

    Widget adicioando como target

---
+++ Pilgrim iOS Widget/Pilgrim_iOS_Widget.swift
@@ -0,0 +1,68 @@
+//
+//  Pilgrim_iOS_Widget.swift
+//  Pilgrim iOS Widget
+//
+//  Created by Luiz Sena on 23/06/23.
+//
+
+import WidgetKit
+import SwiftUI
+import Intents
+
+struct Provider: IntentTimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+    }
+
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        completion(entry)
+    }
+
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let configuration: ConfigurationIntent
+}
+
+struct Pilgrim_iOS_WidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
+
+struct Pilgrim_iOS_Widget: Widget {
+    let kind: String = "Pilgrim_iOS_Widget"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            Pilgrim_iOS_WidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Pilgrim Score Counter")
+        .description("This is an example widget.")
+    }
+}
+
+struct Pilgrim_iOS_Widget_Previews: PreviewProvider {
+    static var previews: some View {
+        Pilgrim_iOS_WidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
---
+++ Pilgrim iOS Widget/Pilgrim_iOS_WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Pilgrim_iOS_WidgetBundle.swift
+//  Pilgrim iOS Widget
+//
+//  Created by Luiz Sena on 23/06/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Pilgrim_iOS_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Pilgrim_iOS_Widget()
+    }
+}
