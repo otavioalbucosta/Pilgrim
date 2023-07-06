441ca0c99f0deeb7dd78a6cbcedabc1128c3b524
Luiz Sena <senaluiz1307@gmail.com>
2023-07-06 20:00:12 -0300

    onboarding responsivo para cada dispositivo

--- Pilgrim/View/Onboarding/FinalOnboardingPageView.swift
+++ Pilgrim/View/Onboarding/FinalOnboardingPageView.swift
@@ -15,42 +15,45 @@ struct FinalOnboardingPageView: View {
     var description: String
 
     var body: some View {
-        VStack {
-            Image(image)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 200)
-            
-            Text(String(localized: String.LocalizationValue(title)))
-                .font(.system(size: 35,weight: .heavy, design: .rounded))
-                .padding(.bottom,12)
-            
-            Text(String(localized: String.LocalizationValue(description)))
-                .font(.system(size: 18, weight: .light, design: .rounded))
-                .padding([.bottom, .leading, .trailing], 30)
-            
-            Button {
-                withAnimation {
-                    finalPage.toggle()
-                }
-            } label: {
-                Image("travelButton")
+        GeometryReader { geo in
+            VStack {
+                Image(image)
                     .resizable()
-                    .frame(width: 250, height: 80)
-                    .overlay {
-                        Text("EMBARCAR")
-                            .font(.system(size: 22, weight: .semibold, design: .rounded))
-                            .foregroundColor(.white)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: geo.size.width * 0.4)
+
+                Text(String(localized: String.LocalizationValue(title)))
+                    .font(.system(size: geo.size.width * 0.08, weight: .heavy, design: .rounded))
+                    .padding(.bottom,12)
+
+                Text(String(localized: String.LocalizationValue(description)))
+                    .font(.system(size: geo.size.width * 0.04 , weight: .light, design: .rounded))
+                    .padding([.bottom, .leading, .trailing], 30)
+
+                Button {
+                    withAnimation {
+                        finalPage.toggle()
                     }
-                    .shadow(radius: 5)
-                    .scaleEffect(idle ? 1.05 : 1)
-                    .animation(Animation.spring().repeatForever().delay(0.3), value: idle)
+                } label: {
+                    Image("travelButton")
+                        .resizable()
+                        .frame(width: geo.size.width * 0.35, height: geo.size.width * 0.15)
+                        .overlay {
+                            Text("EMBARCAR")
+                                .font(.system(size: geo.size.width * 0.05, weight: .semibold, design: .rounded))
+                                .foregroundColor(.white)
+                        }
+                        .shadow(radius: 5)
+                        .scaleEffect(idle ? 1.05 : 1)
+                        .animation(Animation.spring().repeatForever().delay(0.3), value: idle)
+                }
             }
-        }
-        .multilineTextAlignment(.center)
-        .foregroundColor(Color(red: 27/255, green: 42/255, blue: 108/255))
-        .onAppear {
-            idle = true
+            .frame(width: geo.size.width, height: geo.size.height)
+            .multilineTextAlignment(.center)
+            .foregroundColor(Color(red: 27/255, green: 42/255, blue: 108/255))
+            .onAppear {
+                idle = true
+            }
         }
     }
 }
